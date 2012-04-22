b76a668ff0f4a269646731ce0930f10754aa4773
Lukas Larson <lukaslarson@gmail.com>
2012-04-22 20:36:05 +0200

    20120422

---
+++ src/com/luklar9/assignment4/StartMenu.java
@@ -0,0 +1,15 @@
+package com.luklar9.assignment4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartMenu extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
